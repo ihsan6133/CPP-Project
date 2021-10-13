25408f812177b9dd469f043be97c8ea57af62e49
Ihsan <mihsan6133@gmail.com>
2021-10-13 13:59:36 +0530

    Added Vector Class

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Vector{
+public:
+  double x;
+  double y;
+  double z;
+  Vector(double _x, double _y, double _z): x(_x),y(_y),z(_z){}
+  void PrintVector(){
+    std::cout << "x: " << x << std::endl;
+    std::cout << "y: " << y << std::endl;
+    std::cout << "z: " << z << std::endl;
+  }
+};
+
 int main(){
-  std::cout << "Hello World";
+  Vector myvector = {3, 5, 2};
+  myvector.PrintVector();
 }
